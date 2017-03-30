d1928c7af6bd24bde67856f34a527ef6d104f0ad
Logan Klein <lkleindesigns@gmail.com>
2017-03-29 19:24:43 -0500

    add mongo lab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes    =    require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+// mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://lklein:kwik1@ds147070.mlab.com:47070/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
